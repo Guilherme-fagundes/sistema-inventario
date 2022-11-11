a840c89052e05f9d57dfd9b466e0a968b8d38f11
Guilherme-fagundes <guilhermekfagundes259@gmil.com>
2022-11-11 20:10:19 -0300

    criando view index do produto  No ramo criando-pagina-produto  Mudanças a serem submetidas: 	new file:   resources/views/admin/produto/index.blade.php

---
+++ resources/views/admin/produto/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.inc.templates.layout')
+
+@section('main')
+<section class="sessProduct">
+    <div class="py-2 product-header">
+        <h3><span class="icon-price-tag"></span> produtos</h3>
+
+    </div>
+
+
+</section>
+@endsection
