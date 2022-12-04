328a992c3eaf3853536eceea152421b3d35599fe
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-12-04 19:24:34 -0300

    Criando template novo prodeuto

---
+++ resources/views/admin/produto/novo.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.inc.templates.layout')
+
+@section('main')
+<section class="sessProduct">
+    <div class="py-2 product-header">
+        <h3><span class="icon-price-tag"></span> Novo produto</h3>
+
+        <a href="#" class="btn btn-outline-success btn-sm"><span class="icon icon-arrow-left"></span> Voltar</a>
+
+    </div>
+
+
+</section>
+@endsection
