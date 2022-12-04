41168694d512f5037f69dad45800bc0867e270bb
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-12-04 18:08:58 -0300

    Criando pagina de listagem de produtos

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.produto.index', [
+            'title' => env('APP_NAME') . ' | Produtos'
+        ]);
+    }
 }
