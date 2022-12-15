d2f42fc882ceeda4c2ca80e4e7b1679a0e4e3c64
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-12-15 14:14:43 -0300

    Criando migration de usuarios

---
+++ database/migrations/2022_12_15_164700_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email', 150);
+            $table->string('pass', 255);
+            $table->string('level');
+            $table->string('status');
+            $table->ipAddress('ip');
+            $table->timestamp('email_virified_at')->nullable();
+            $table->timestamp('created_at')->defaut(DB::raw("CURRENT_TIMESTAMP"));
+            $table->timestamp('updated_at')->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
